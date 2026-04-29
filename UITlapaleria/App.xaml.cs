ac1d1b20bd2fb471d081c26fce27200e4502a872
Santi4567 <santiantozac345@gmail.com>
2026-04-29 13:17:52 -0600

    Agregar archivos de proyecto.

---
+++ UITlapaleria/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using dotenv.net;
+
+namespace UITlapaleria
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // Este método se ejecuta automáticamente antes de abrir cualquier ventana
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // 2. Cargamos el archivo .env con el método de dotenv.net
+            DotEnv.Load();
+        }
+    }
+
+}
---
+++ UITlapaleria/ViewModels/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿//Clase padre encargado de los Estados dentro de la Interfaz de Usuario (Equivalente al los UseStade de React)
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UITlapaleria.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        // 1. El evento que WPF escucha
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // 2. Método que dispara el evento
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // 3. "setState" personalizado
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
